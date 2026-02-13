817d0b326b4d592cc03b0097f43c2d0c7c45e258
P-Alakara <alakara11728@gmail.com>
2026-02-13 20:59:40 +0300

    Verify comment function

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -83,6 +83,19 @@ def add_comment(request, pk):
 
     return redirect("post-detail", pk=post.pk)
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs.get("pk")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
