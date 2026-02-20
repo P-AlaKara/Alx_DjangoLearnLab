9daef0a5bef3fd272392c2deee56accfc1c85d63
P-Alakara <alakara11728@gmail.com>
2026-02-20 20:24:22 +0300

    implement user follows and feed

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from rest_framework.decorators import action
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.views import APIView
 
 User = get_user_model()
 
@@ -48,3 +52,36 @@ class ProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def follow(self, request, pk=None):
+        target_user = self.get_object()
+
+        if target_user == request.user:
+            return Response({"error": "You cannot follow yourself."}, status=400)
+
+        request.user.following.add(target_user)
+        return Response({"message": f"You are now following {target_user.username}."})
+
+    @action(detail=True, methods=['post'])
+    def unfollow(self, request, pk=None):
+        target_user = self.get_object()
+        request.user.following.remove(target_user)
+        return Response({"message": f"You unfollowed {target_user.username}."})
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
